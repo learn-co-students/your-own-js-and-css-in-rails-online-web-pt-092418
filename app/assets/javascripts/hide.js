fba50d6fd90b924f029c8bb685ed5aec8a21111c
github username <github email address>
2019-05-01 21:39:14 -0400

    Done.

---
+++ app/assets/javascripts/hide.js
@@ -0,0 +1,8 @@
+function hideWhenClicked(e){
+  // debugger
+  return this.hidden = true;
+}
+
+$('#hide_this').click(hideWhenClicked)
+
+// document.getElementById('hide_this').click(hideWhenClicked)
